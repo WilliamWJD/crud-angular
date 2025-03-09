c92716488d231d0dfbfcc57793a45fa1a205d506
wildias <wildias@brasilseg.com.br>
2025-03-09 11:15:00 -0300

    chore: iniciando projeto

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Tarefa } from '../../../Tarefa';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-item',
+  imports: [FontAwesomeModule, CommonModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css',
+})
+export class TaskItemComponent {
+  @Input() tarefa!: Tarefa;
+
+  faTimes = faTimes;
+}
