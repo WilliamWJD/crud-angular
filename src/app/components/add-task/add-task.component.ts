83448fddc77478a94bdc90982288bf8b83886d0f
wildias <wildias@brasilseg.com.br>
2025-03-09 12:35:51 -0300

    feat: abrir ou fechar form button

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Tarefa } from '../../../Tarefa';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-add-task',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ButtonComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css',
 })
@@ -15,6 +16,8 @@ export class AddTaskComponent {
   tarefa: string = '';
   categoria: string = '';
   concluido: boolean = false;
+
+  mostrarAddTarefa: boolean = false;
 
   onSubmit() {
     if (!this.tarefa) {
@@ -33,5 +36,9 @@ export class AddTaskComponent {
     this.tarefa = '';
     this.categoria = '';
     this.concluido = false;
+  }
+
+  alteraVisualizacao(valor: boolean) {
+    this.mostrarAddTarefa = valor;
   }
 }
